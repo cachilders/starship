b691c17ae3081f937d3685e8228f33436e1c1c65
Casey Childers <cachilders@gmail.com>
2018-01-14 23:37:17 -0800

    [bug in] broken redux implementation

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,29 @@
 import 'isomorphic-unfetch'
 import React, { Fragment } from 'react'
+import { bindActionCreators } from 'redux'
+import withRedux from 'next-redux-wrapper'
+import { initStore, getStars, sortStars } from '../store'
 import Head from '../components/head'
 import Selector from '../components/selector'
 import Star from '../components/star'
 import Styles from '../components/styles'
 
-export default class extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      username: 'cachilders' // Temporary, natch
-    };
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  async setStars() {
-    const res = await fetch(`http://localhost:8080/stars?username=${this.state.username}`) // Temporary
-    const json = await res.json()
-    const stars = [...json]
-    const sortBy = 'starred'
-    this.setState({ sortBy, stars, raw: stars.slice() })
-  }
-
-  componentDidMount() {
-    this.setStars()
-  }
-
-  handleChange(e) {
-    const sortBy = e.target.value
-    this.setState({
-      sortBy: sortBy,
-      stars: sortBy === 'starred' ? this.state.raw : this.state.stars.sort((a, b) => {
-        if (sortBy === 'alpha') {
-          const aName = a.name.toLowerCase()
-          const bName = b.name.toLowerCase()
-          if (aName > bName) {
-            return +1
-          } else if (aName < bName) {
-            return -1
-          } else {
-            return 0
-          }
-        } else if (sortBy === 'updatedAt') {
-          const aDate = new Date(a.updated_at)
-          const bDate = new Date(b.updated_at)
-          return bDate - aDate
-        } else if (sortBy === 'language') {
-          const aLang = a.language ? a.language.toLowerCase() : 'N/A'
-          const bLang = b.language ? b.language.toLowerCase() : 'N/A'
-          if (aLang > bLang) {
-            return +1
-          } else if (aLang < bLang) {
-            return -1
-          } else {
-            return 0
-          }
-        }
-      }),
-    })
+class Main extends React.Component {
+  static getInitialProps ({ store, isServer }) {
+    store.dispatch(getStars())
   }
 
   render() {
+    const { sortBy, stars, sortStars } = this.props
+    console.log(sortBy, stars)
     return (
       <Fragment>
         <Head />
-        <Selector handleChange={ this.handleChange } sortBy={ this.state.sortBy } />
+        <Selector handleChange={ e => sortStars(e.target.value) } sortBy={ sortBy } />
         { 
-          this.state.stars ? this.state.stars
+          stars ? stars
             .map(
-              star => star.private ? null : <Star key={ star.id } star={ star } sortBy={ this.state.sortBy } />
+              star => star.private ? null : <Star key={ star.id } star={ star } sortBy={ sortBy } />
             ) : null
         }
         <Styles />
@@ -76,3 +31,19 @@ export default class extends React.Component {
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    sortBy: state.sortBy,
+    stars: state.stars,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getStars: bindActionCreators(getStars, dispatch),
+    sortStars: bindActionCreators(sortStars, dispatch)
+  }
+}
+
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Main)
---
+++ store.js
@@ -0,0 +1,89 @@
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { default as thunkMiddleware } from 'redux-thunk'
+
+const initialState = {
+  username: 'cachilders', // Temporary, natch
+  sortBy: 'starred',
+  stars: [],
+  raw: [],
+}
+
+// const thunk = store => {  
+//   const dispatch = store.dispatch
+//   const getState = store.getState
+
+//   return next => action => {
+//     if (typeof action === 'function') {
+//       return action(dispatch, getState)
+//     }
+
+//     console.log(action)
+//     return next(action)
+//   }
+// }
+
+export const actionTypes = {
+  SET_SORT: 'SET_SORT',
+  SET_STARS: 'SET_STARS',
+}
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case action.type.SET_SORT:
+      return Object.assign({}, state, { sortBy: action.sortBy, stars: action.stars })
+    case action.type.SET_STARS:
+      return Object.assign({}, state, { stars: action.stars, raw: action.stars.slice() })
+    default:
+      return state
+  }
+}
+
+export const getStars = () => {
+  return async (dispatch, getState) => {
+    const res = await fetch(`http://localhost:8080/stars?username=${getState().username}`) // Temporary
+    const json = await res.json()
+    const stars = [...json]
+    return dispatch({ type: actionTypes.SET_STARS, stars })
+  }
+}
+
+export const sortStars = (sortBy) => {
+  return (dispatch, getState) => {
+    return dispatch({
+      type: actionTypes.SET_SORT,
+      sortBy,
+      stars: sortBy === 'starred' ? getState().raw : getState().stars.sort((a, b) => {
+        if (sortBy === 'alpha') {
+          const aName = a.name.toLowerCase()
+          const bName = b.name.toLowerCase()
+          if (aName > bName) {
+            return +1
+          } else if (aName < bName) {
+            return -1
+          } else {
+            return 0
+          }
+        } else if (sortBy === 'updatedAt') {
+          const aDate = new Date(a.updated_at)
+          const bDate = new Date(b.updated_at)
+          return bDate - aDate
+        } else if (sortBy === 'language') {
+          const aLang = a.language ? a.language.toLowerCase() : 'N/A'
+          const bLang = b.language ? b.language.toLowerCase() : 'N/A'
+          if (aLang > bLang) {
+            return +1
+          } else if (aLang < bLang) {
+            return -1
+          } else {
+            return 0
+          }
+        }
+      }),
+    })
+  }
+}
+
+export const initStore = (initialState) => {
+  return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)))
+}
