2fb9c0fc8ccf618ffd32c45c8c9b2f72d7b2e020
Casey Childers <cachilders@gmail.com>
2018-01-20 21:52:51 -0800

    interpret colon string emoji as unicode

--- components/star.js
+++ components/star.js
@@ -1,4 +1,5 @@
 import { List } from 'semantic-ui-react'
+import { getUnicode } from 'emoji-dictionary';
 
 export default ({ star, sortBy }) =>
   <List.Item>
@@ -8,6 +9,10 @@ export default ({ star, sortBy }) =>
         { sortBy === 'language' && star.language ? <span>{ `[${star.language}] ` }</span> : null}
         { star.full_name }
       </List.Header>
-      <List.Description as='a'>{ star.description }</List.Description>
+      {  star.description &&
+        <List.Description>
+          { star.description.replace(/:.*:/g, (str) => getUnicode(str.replace(/:/g, ''))) }
+        </List.Description>
+      }
     </List.Content>
   </List.Item>
