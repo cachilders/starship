5478706aacca3606ed04ab4559ec071b87c140ab
Casey Childers <cachilders@gmail.com>
2018-01-20 23:23:56 -0800

    add live search

--- components/stars-list.js
+++ components/stars-list.js
@@ -1,21 +1,37 @@
 import { Fragment } from 'react'
-import { Divider, List } from 'semantic-ui-react'
+import { Divider, Input, List } from 'semantic-ui-react'
+import Downshift from 'downshift'
 import Selector from './selector'
 import Star from './star'
 
 export default ({ sortBy, stars, sortStars, hasStars }) =>
-  <Fragment>
-    <Selector
-      handleChange={ (e, data) => sortStars(data.value) }
-      sortBy={ sortBy }
-      hasStars={ hasStars }
+    <Downshift
+      render={({
+        getInputProps,
+        getRootProps,
+        inputValue,
+      }) => (
+        <Fragment { ...getRootProps({refKey: 'innerRef'}) } >
+          <Selector
+            handleChange={ (e, data) => sortStars(data.value) }
+            sortBy={ sortBy }
+            hasStars={ hasStars }
+          /> {' '}
+          <Input transparent { ...getInputProps({placeholder: 'Search'}) } />
+          <Divider hidden />
+          <List divided relaxed>
+            { 
+              stars.filter(
+                star =>
+                  !inputValue ||
+                  star.full_name.toLowerCase().includes(inputValue.toLowerCase()) ||
+                  star.description && star.description.toLowerCase().includes(inputValue.toLowerCase())
+              )
+              .map(
+                star => star.private ? null : <Star key={ star.id } star={ star } sortBy={ sortBy } />
+              )
+            }
+          </List>
+        </Fragment>
+      )}
     />
-    <Divider hidden />
-    <List divided relaxed>
-      { 
-        stars.map(
-          star => star.private ? null : <Star key={ star.id } star={ star } sortBy={ sortBy } />
-        )
-      }
-    </List>
-  </Fragment>
